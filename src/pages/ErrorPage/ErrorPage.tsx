7cdd0160860d673f1c33452416c151b2e529b70a
darya-yarosh <gurimusagi@yandex.ru>
2024-01-19 20:57:03 +0300

    Created error page.

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router';
+
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
+
+import './ErrorPage.css';
+
+export default function ErrorPage() {
+    const navigate = useNavigate();
+
+    return <>
+        <Header />
+        <div className="ErrorPage_wrapper">
+            <div className="ErrorPage_body">
+                <header>
+                    <h1>Something went wrong.</h1>
+                    <p>Current address is invalid.</p>
+                </header>
+                <nav>
+                    <button onClick={() => navigate(-1)}>Go to previous page</button>
+                    <button onClick={() => navigate('/')}>Go to main page</button>
+                </nav>
+            </div>
+        </div>
+        <Footer />
+    </>
+}
