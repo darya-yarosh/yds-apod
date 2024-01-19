b7f8890647a9f67561edbc4b98efed78c274a35c
darya-yarosh <gurimusagi@yandex.ru>
2024-01-19 20:50:31 +0300

    Created footer component.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import './Footer.css';
+
+export default function Footer() {
+    return <footer className="Footer_wrapper">
+        <span className="Footer_author">Created by YDS, 2024</span>
+        <a className="Footer_api" href="https://api.nasa.gov">API</a>
+    </footer>
+}
