cdc9e43bfea6ee149a83b72376ba64945df009dd
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-01-10 00:37:15 +0300

    [APOD/format]: Format the ErrorPage code.

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,22 +1,49 @@
 import { useNavigate } from 'react-router';
+import { useCallback } from 'react';
 
 import './ErrorPage.css';
 
 export default function ErrorPage() {
     const navigate = useNavigate();
 
+    /**
+     * Handlers
+     */
+    const handleNavToPrevPage = useCallback(() => {
+        navigate(-1);
+    }, [navigate]);
+
+    const handleNavToMainPage = useCallback(() => {
+        navigate('/')
+    }, [navigate]);
+
+    /**
+     * Renders
+     */
+    const renderHeader = useCallback(() => {
+        return (
+            <header>
+                <h1>Something went wrong.</h1>
+                <p>Current address is invalid.</p>
+                <p>Try changing the date or follow these steps.</p>
+            </header>
+        );
+    }, []);
+
+    const renderNav = useCallback(() => {
+        return (
+            <nav>
+                <button onClick={handleNavToPrevPage} type="button">Go to previous page</button>
+                <button onClick={handleNavToMainPage} type="button">Go to main page</button>
+            </nav>
+        );
+    }, [handleNavToPrevPage, handleNavToMainPage]);
+
     return <>
         <div className="ErrorPage_wrapper">
             <div className="ErrorPage_body">
-                <header>
-                    <h1>Something went wrong.</h1>
-                    <p>Current address is invalid.</p>
-                    <p>Try changing the date or follow these steps.</p>
-                </header>
-                <nav>
-                    <button onClick={() => navigate(-1)}>Go to previous page</button>
-                    <button onClick={() => navigate('/')}>Go to main page</button>
-                </nav>
+                {renderHeader()}
+                {renderNav()}
             </div>
         </div>
     </>
